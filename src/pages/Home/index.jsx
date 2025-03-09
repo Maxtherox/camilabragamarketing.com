4e84bd695070261a05138362c10dd9d11ad8a68d
Guilherme <gui-rock02@Hotmail.com>
2025-03-09 01:53:38 -0300

    feat(home): seção 01 da home adicionada a estrutura HTML

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,28 @@
-import { Container } from "./styles";
+import { Container, SectionOne, Branding, Title, ButtonBorder } from "./styles";
+import imgOne from "../../assets/img1.png"
 
 export function Home(){
     return(
         <Container>
-            <h1>Teste</h1>
+            <SectionOne>  
+            <div>
+
+            </div>
+                <Branding>
+                    <h2>Camilla Braga</h2>
+                    <span>Social media</span>
+                </Branding>
+                <Title>
+                    <h1>
+                    Apresente o seu negócio com estratégia e criatividade
+                    </h1>
+                    <p>
+                    Sou Camila Braga, especialista em social media. Meu propósito é ajudar empresas e empreendedores a conquistar resultados reais com estratégias criativas, gestão eficiente e conteúdos de impacto.
+                    </p>
+                </Title>
+                 <ButtonBorder>Quero saber mais</ButtonBorder>
+            </SectionOne>
+            <img src={imgOne}/>
         </Container>
     )
 }
