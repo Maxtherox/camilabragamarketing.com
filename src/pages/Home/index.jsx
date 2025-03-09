66d4e7c776c50da1828b2434d0e751822f00f64a
Guilherme <gui-rock02@hotmail.com>
2025-03-09 20:49:06 -0300

    style(home): sectionOne estilizado.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,21 +8,23 @@ export function Home(){
             <div>
 
             </div>
+               
+                <Title>
                 <Branding>
                     <h2>Camilla Braga</h2>
                     <span>Social media</span>
                 </Branding>
-                <Title>
                     <h1>
                     Apresente o seu negócio com estratégia e criatividade
                     </h1>
                     <p>
                     Sou Camila Braga, especialista em social media. Meu propósito é ajudar empresas e empreendedores a conquistar resultados reais com estratégias criativas, gestão eficiente e conteúdos de impacto.
                     </p>
+                    <ButtonBorder>Quero saber mais</ButtonBorder>
                 </Title>
-                 <ButtonBorder>Quero saber mais</ButtonBorder>
+                 <img src={imgOne}/>
             </SectionOne>
-            <img src={imgOne}/>
+           
         </Container>
     )
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -5,14 +5,41 @@ export const Container = styled.main`
 `
 
 export const SectionOne = styled.section`
-
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+> img {
+    margin-top: 4.6rem;
+    margin-bottom: 4.6rem;
+}
 `
 export const Branding = styled.section`
 
 `
 export const Title = styled.div`
+display: flex;
+flex-direction: column;
+
+gap: 1rem;
+max-width: 43.4rem;
+justify-content: center;
 `
 
 export const ButtonBorder = styled.a`
+display: flex;
+justify-content: center;
+align-items: center;
+border: 2px solid ${({ theme }) => theme.COLORS.BROWN};
+border-radius: 3.2rem;
+font-weight: bold;
+font-size: 1.4rem;
+width: 16.1rem;
+height: 5.7rem;
+margin-top: 2rem;
+`
+
+// seção 2
+
+export const SectionTwo = styled.section`
 
 `
